9f320bb8ebf77aeb4b48ad0c611532a0753718c9
Robertfbs <robertfbs@gmail.com>
2023-06-25 17:59:45 -0300

    Aula 4 - Criando Objetos e Classes em JavaScript

--- aula4.js
+++ aula4.js
@@ -1,26 +1,28 @@
-const precoGasolina = 5.30;
-const precoEtanol = 4.10;
-const kmPorLitro = 10;
-const trajetoEmkm = 100;
-const tipoCombustivel = 'Etanol';
+// APRENDENDO A CALCULAR GASTO DE LT POR KM NUMMA VIAGEM.
+
+// const precoGasolina = 5.30;
+// const precoEtanol = 4.10;
+// const kmPorLitro = 10;
+// const trajetoEmkm = 100;
+// const tipoCombustivel = 'Etanol';
 
 
 
-const litrosConsumidos = trajetoEmkm / kmPorLitro;
+// const litrosConsumidos = trajetoEmkm / kmPorLitro;
 
 
 
-if (tipoCombustivel === 'Etanol') {
-    const gastoViagemIda = precoEtanol * litrosConsumidos;
-    const idaVoltaViagem = gastoViagemIda * 2;
-    console.log(gastoViagemIda.toFixed(2));
-    console.log(idaVoltaViagem.toFixed(2))
-} else{
-    const gastoViagemIda = precoGasolina * litrosConsumidos;
-    const idaVoltaViagem = gastoViagemIda * 2;
-    console.log(gastoViagemIda.toFixed(2));
-    console.log(idaVoltaViagem.toFixed(2));
-}
+// if (tipoCombustivel === 'Etanol') {
+//     const gastoViagemIda = precoEtanol * litrosConsumidos;
+//     const idaVoltaViagem = gastoViagemIda * 2;
+//     console.log(gastoViagemIda.toFixed(2));
+//     console.log(idaVoltaViagem.toFixed(2))
+// } else{
+//     const gastoViagemIda = precoGasolina * litrosConsumidos;
+//     const idaVoltaViagem = gastoViagemIda * 2;
+//     console.log(gastoViagemIda.toFixed(2));
+//     console.log(idaVoltaViagem.toFixed(2));
+// }
 
 
 
--- exercicio2.js
+++ exercicio2.js
@@ -1,17 +1,42 @@
-const precoEtiqueta = 100;
-const formaDePagamento = 1;
+// CÓDIGO BASE PARA CALCULAR PREÇO DO PRODUTO CONFORME ESCOLHA DO PAGAMENTO PELO USUÁRIO, ADICIONANDO JUROS OU DANDO DESCONTOS.
 
-if(formaDePagamento === 1){
-    console.log(precoEtiqueta - (precoEtiqueta * 0.1));
-}else if(formaDePagamento === 2){
-    console.log(precoEtiqueta - (precoEtiqueta * 0.15))
-}else if(formaDePagamento === 3){
-    console.log(precoEtiqueta)
-}else{
-    console.log(precoEtiqueta + (precoEtiqueta * 0.1))
+
+// const precoEtiqueta = 100;
+// const formaDePagamento = 1;
+
+// if(formaDePagamento === 1){
+//     console.log(precoEtiqueta - (precoEtiqueta * 0.1));
+// }else if(formaDePagamento === 2){
+//     console.log(precoEtiqueta - (precoEtiqueta * 0.15))
+// }else if(formaDePagamento === 3){
+//     console.log(precoEtiqueta)
+// }else{
+//     console.log(precoEtiqueta + (precoEtiqueta * 0.1))
+// }
+
+
+// MESMO EXERCÍCIO, AGORA UTILIZANDO "FUNCTION".
+
+function aplicarDesconto(valor, desconto){
+    return (valor - (valor * desconto)); 
 }
 
+function aplicarJuros(valor, juros){
+    return (valor + (valor * juros))
+}
+const precoEtiqueta = 100;
+const formaDePagamento = 100;
 
 
 
+    if(formaDePagamento === 1){
+        console.log(aplicarDesconto(precoEtiqueta, 0.1));
+    }else if(formaDePagamento === 2){
+        console.log(aplicarDesconto(precoEtiqueta,0.15))
+    }else if(formaDePagamento === 3){
+        console.log(precoEtiqueta)
+    }else{
+        console.log(aplicarJuros(precoEtiqueta, 0.1))
+    }
 
+   
